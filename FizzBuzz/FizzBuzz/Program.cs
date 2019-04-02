871f420bca2262046d22c366aebba88ee7860e7d
Samuel Smith <stsmith313@gmail.com>
2019-04-01 20:22:49 -0400

    Created the FizzBuzz console app to help practice possible questions for interviews.

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 101; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
