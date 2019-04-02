afd827f2ae3e199318a013f48fbd56eec60781a0
Samuel Smith <stsmith313@gmail.com>
2019-04-01 20:25:13 -0400

    Added some comments for more clarity of what the program is doing

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -12,19 +12,19 @@ namespace FizzBuzz
         {
             for(int i = 0; i < 101; i++)
             {
-                if(i % 3 == 0 && i % 5 == 0)
+                if(i % 3 == 0 && i % 5 == 0) //if there is a multiple of 3 and 5 then it will output fizzbuzz
                 {
                     Console.WriteLine("FizzBuzz");
                 }
-                else if(i % 3 == 0)
+                else if(i % 3 == 0) //if it is a multiple of 3 then we will output fizz
                 {
                     Console.WriteLine("Fizz");
                 }
-                else if(i % 5 == 0)
+                else if(i % 5 == 0) //if the number is a multiple of 5 then we will output buzz
                 {
                     Console.WriteLine("Buzz");
                 }
-                else
+                else //otherwise we will output the number that we are at
                 {
                     Console.WriteLine(i);
                 }
